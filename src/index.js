b286d2a74e7b411dbde73b616b8dd576c7186154
Manoj Garu <manojgaru764@gmail.com>
2022-08-05 23:21:14 +0530

    3

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Modal from "react-modal";
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter,Router} from 'react-router-dom'
 Modal.setAppElement("#root");
 
 ReactDOM.render(
-  <BrowserRouter basename="/"><App /></BrowserRouter>,
+  <BrowserRouter ><App /></BrowserRouter>,
   document.getElementById("root")
 );
