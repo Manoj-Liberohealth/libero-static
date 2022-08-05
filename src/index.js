9671d5e296ec51c3f9bfa5d3bcab4d7efedd8102
Manoj Garu <manojgaru764@gmail.com>
2022-08-06 01:22:52 +0530

    6

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Modal from "react-modal";
-import {BrowserRouter,HashRouter} from 'react-router-dom'
+import {createBrowserHistory} from "history"
+import {BrowserRouter,HashRouter,unstable_HistoryRouter as HistoryRouter} from 'react-router-dom'
 Modal.setAppElement("#root");
 
+const hist = createBrowserHistory({ window });
+
+
 ReactDOM.render(
-  <HashRouter ><App /></HashRouter>,
+  <HistoryRouter history={hist}><App /></HistoryRouter>,
   document.getElementById("root")
 );
